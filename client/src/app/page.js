46669a9ead43dde8a98288ac36db3e59018809a4
Eric M <ern.martin12@gmail.com>
2024-11-28 14:10:47 -0500

    loader change

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -26,6 +26,7 @@ export default function Home() {
   const [sortedBuildings, setSortedBuildings] = useState([]);
   const [day, setDay] = useState("");
   const [loading, setLoading] = useState(true);
+  const [locationFetched, setLocationFetched] = useState(false); // New state to track location fetch
   const [sortOption, setSortOption] = useState("Closest");
   const [filterOption, setFilterOption] = useState("All");
   const [displayedBuildings, setDisplayedBuildings] = useState([]);
@@ -46,17 +47,20 @@ export default function Home() {
           (position) => {
             const { latitude, longitude } = position.coords;
             setCoordinates([latitude, longitude]);
+            setLocationFetched(true); // Set locationFetched to true when location is fetched
           },
           () => {
             // User denied location sharing, fallback to default coordinates
             setCoordinates(null);
             setSortOption("Name");
+            setLocationFetched(true); // Set locationFetched to true if location is denied
           }
         );
       } else {
         // Geolocation not supported, fallback to default coordinates
         setCoordinates(null);
         setSortOption("Name");
+        setLocationFetched(true); // Set locationFetched to true if geolocation is not supported
       }
     };
 
@@ -83,119 +87,120 @@ export default function Home() {
       setDisplayedBuildings(updatedBuildings);
     };
 
-    fetchData();
+    if (locationFetched) {
+      fetchData();
+      setTimeout(() => {
+        setLoading(false);
+      }, "2000");
+    }
+  }, [coordinates, locationFetched]);
 
-    setTimeout(() => {
-      setLoading(false);
-    }, "2000");
-  }, [coordinates]);
-
-  if (!loading) {
-    return (
-      <main className="relative h-screen text-white">
-        {/* Fullscreen Map */}
-        <Map
-          data={sortedBuildings}
-          className="absolute inset-0 w-full h-full"
-          coordinates={coordinates}
-          selectedCoordinates={selectedCoordinates} // Pass down
-        />
-
-        {/* Sidebar */}
-        <div className="absolute border-r-[1px] border-neutral-600 bottom-0 left-0 right-0 h-[60%] sm:h-full sm:w-1/3 lg:w-1/4 bg-zinc-800/70 backdrop-blur-sm shadow-lg z-10 flex flex-col">
-          {/* Header */}
-          <div className="w-full  py-2 pl-4 sm:py-4 sm:pt-8 sm:pl-8 gap-4 hidden sm:flex flex-col">
-            <Logo />
-
-            <AnimatedDateTime />
-            <div className="flex flex-wrap gap-2">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline">
-                    <ArrowDown size="10px" /> {sortOption}
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  align="start"
-                  className="bg-neutral-900 text-white border-0"
-                >
-                  <DropdownMenuLabel>Sort By:</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem
-                    onClick={() => setSortOption("Closest")}
-                    disabled={!coordinates}
-                  >
-                    Closest
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    onClick={() => setSortOption("Furthest")}
-                    disabled={!coordinates}
-                  >
-                    Furthest
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    onClick={() => setSortOption("Highest Rated")}
-                  >
-                    Highest Rated
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setSortOption("Name")}>
-                    Name
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline">
-                    <ListFilter size="10px" /> {filterOption}
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  align="start"
-                  className="bg-neutral-900 text-white border-0"
-                >
-                  <DropdownMenuLabel>Filter By:</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem onClick={() => setFilterOption("All")}>
-                    All
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setFilterOption("Open")}>
-                    Open
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setFilterOption("Closed")}>
-                    Closed
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </div>
-
-          {/* Scrollable List */}
-          <ScrollArea className="flex-grow overflow-y-auto ">
-            <div className="block sm:hidden p-2">
-              <Logo />
-            </div>
-            <div className="p-2 sm:p-4">
-              {displayedBuildings.map((building, index) => (
-                <BuildingCard
-                  building={building}
-                  coordinates={coordinates}
-                  key={index}
-                  day={day}
-                  id={building.name}
-                  onClick={() => setSelectedCoordinates(building.coords)} // Pass building.coords
-                />
-              ))}
-            </div>
-          </ScrollArea>
-        </div>
-      </main>
-    );
-  } else {
+  if (loading || !locationFetched) {
     return (
       <div className="min-h-screen bg-zinc-900 flex justify-center items-center">
         <Loader />
       </div>
     );
   }
+
+  return (
+    <main className="relative h-screen text-white">
+      {/* Fullscreen Map */}
+      <Map
+        data={sortedBuildings}
+        className="absolute inset-0 w-full h-full"
+        coordinates={coordinates}
+        selectedCoordinates={selectedCoordinates} // Pass down
+      />
+
+      {/* Sidebar */}
+      <div className="absolute border-r-[1px] border-neutral-600 bottom-0 left-0 right-0 h-[60%] sm:h-full sm:w-1/3 lg:w-1/4 bg-zinc-800/70 backdrop-blur-sm shadow-lg z-10 flex flex-col">
+        {/* Header */}
+        <div className="w-full py-2 pl-4 sm:py-4 sm:pt-8 sm:pl-8 gap-4 hidden sm:flex flex-col">
+          <Logo />
+
+          <AnimatedDateTime />
+          <div className="flex flex-wrap gap-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  <ArrowDown size="10px" /> {sortOption}
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="start"
+                className="bg-neutral-900 text-white border-0"
+              >
+                <DropdownMenuLabel>Sort By:</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onClick={() => setSortOption("Closest")}
+                  disabled={!coordinates}
+                >
+                  Closest
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setSortOption("Furthest")}
+                  disabled={!coordinates}
+                >
+                  Furthest
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setSortOption("Highest Rated")}
+                >
+                  Highest Rated
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setSortOption("Name")}>
+                  Name
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  <ListFilter size="10px" /> {filterOption}
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="start"
+                className="bg-neutral-900 text-white border-0"
+              >
+                <DropdownMenuLabel>Filter By:</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => setFilterOption("All")}>
+                  All
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setFilterOption("Open")}>
+                  Open
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setFilterOption("Closed")}>
+                  Closed
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+
+        {/* Scrollable List */}
+        <ScrollArea className="flex-grow overflow-y-auto ">
+          <div className="block sm:hidden p-2">
+            <Logo />
+          </div>
+          <div className="p-2 sm:p-4">
+            {displayedBuildings.map((building, index) => (
+              <BuildingCard
+                building={building}
+                coordinates={coordinates}
+                key={index}
+                day={day}
+                id={building.name}
+                onClick={() => setSelectedCoordinates(building.coords)} // Pass building.coords
+              />
+            ))}
+          </div>
+        </ScrollArea>
+      </div>
+    </main>
+  );
 }
