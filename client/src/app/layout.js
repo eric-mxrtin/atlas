807aff259ed92548d0c4d1148e41f6f77c8d031e
Eric M <ern.martin12@gmail.com>
2024-11-30 00:29:48 -0500

    seo optimization

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { DM_Sans } from "next/font/google";
 import { Urbanist } from "next/font/google";
+import Head from "next/head";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -17,6 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content="A campus navigation tool for students at the University of Toronto." />
+        <meta name="keywords" content="Toronto, university, campus, maps, buildings, libraries, mapbox" />
+        <meta name="author" content="Eric Martin" />
+        <meta property="og:title" content="atlas by eric martin" />
+        <meta property="og:description" content="A campus navigation tool for students at the University of Toronto. Discover open buildings and libraries, their nearest subway stations, and their star ratings from Google. Made with Nextjs, React and Mapbox." />
+      </Head>
       <body
         className={`${dmSans.variable} font-sans`}
         src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/ping.js"
