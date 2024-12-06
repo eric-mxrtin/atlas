73b2e5dca740a675cf42e33db7781bdadc094c16
Eric M <ern.martin12@gmail.com>
2024-12-06 18:07:17 -0500

    .env changes

--- server/api/buildings.js
+++ server/api/buildings.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 const router = express.Router();
 
-// Function to load and encode data
+// load and encode data
 const loadData = () => {
-  const filePath = path.join(__dirname, '../data.json');
+  const filePath = path.join(__dirname, process.env.GOOGLE_MAPS_KEY); // read file path from .env
   const jsonData = fs.readFileSync(filePath, 'utf-8');
-  const encodedData = Buffer.from(jsonData).toString('base64'); // Encode data in base64
+  const encodedData = Buffer.from(jsonData).toString('base64'); // encode data in base64
   return encodedData;
 };
 
-// GET route to fetch data
+// handle get requests to fetch data
 router.get('/', (req, res) => {
   try {
     const encodedData = loadData();
-    res.json({ data: encodedData });  // Return the encoded data
+    res.json({ data: encodedData }); // return the encoded data
   } catch (error) {
-    console.error('Error reading JSON data:', error);
-    res.status(500).send({ error: 'Failed to load data.' });
+    console.error('error reading json data:', error);
+    res.status(500).send({ error: 'failed to load data.' });
   }
 });
 
